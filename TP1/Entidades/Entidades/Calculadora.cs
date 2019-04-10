23c96d5cc3c0eb0a3dfae7c7058ab394f601fe28
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-10 00:13:27 -0300

    Avances pequeño

--- TP1/Entidades/Entidades/Calculadora.cs
+++ TP1/Entidades/Entidades/Calculadora.cs
@@ -12,8 +12,38 @@ namespace Entidades
         {
             string retorno;
 
+            if(operador == "+" || operador == "-" || operador == "/" || operador == "*")
+            {
+                retorno = operador;
+            }else
+            {
+                retorno = "+";
+            }
 
             return retorno;
+        }
+
+        public double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado=0;
+            string auxOperador;
+            auxOperador = Calculadora.ValidarOperador(operador);
+
+            if(auxOperador == "+")
+            {
+                 
+            }else if(auxOperador == "-")
+            {
+
+            }else if(auxOperador == "*")
+            {
+               
+            }else if(auxOperador == "/")
+            {
+                
+            }
+
+            return resultado;
         }
     }
 }
--- TP1/Entidades/Entidades/Numero.cs
+++ TP1/Entidades/Entidades/Numero.cs
@@ -8,5 +8,44 @@ namespace Entidades
 {
     class Numero
     {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+          
+        }
+
+        private double ValidarNumero(string strNumero)
+        {
+            int i;
+            double retorno=1;
+            int flag = 1;
+
+            for(i=0;i<strNumero.Length;i++)
+            {
+                if(strNumero[i] < '0' || strNumero[i] > '9')
+                {
+                    retorno = 0;
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 1)
+            {
+                double.TryParse(strNumero,out retorno);
+            }
+
+            return retorno;
+        }
     }
 }
