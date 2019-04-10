312657492d3757e8551c86ec533273ade5ad5fb4
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-04-09 23:33:32 -0300

    Inicio del TP1

---
+++ TP1/Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Calculadora
+    {
+        private static string ValidarOperador(string operador)
+        {
+            string retorno;
+
+
+            return retorno;
+        }
+    }
+}
