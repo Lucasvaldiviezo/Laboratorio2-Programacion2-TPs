56d02f55e89b3b4e34765129b6aa672b2d858465
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-05-19 11:46:02 -0300

    TP2 Listo

---
+++ TP2/TP-02/Entidades/Leche.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Leche : Producto
+    {
+        public enum ETipo { Entera, Descremada }
+        private ETipo tipo;
+
+        public Leche(EMarca marca, string codigo, ConsoleColor color, ETipo tipo)
+            : base(codigo, marca, color)
+        {
+            this.tipo = tipo;
+        }
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigo"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string codigo, ConsoleColor color)
+            : base(codigo, marca, color)
+        {
+            tipo = ETipo.Entera;
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
