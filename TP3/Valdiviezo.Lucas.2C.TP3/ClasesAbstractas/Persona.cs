a461aa091df25f43e89bcbb154221d1a330ae09f
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-06-05 21:29:47 -0300

    Empezando TP3

---
+++ TP3/Valdiviezo.Lucas.2C.TP3/ClasesAbstractas/Persona.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAbstractas
+{
+    public abstract class Persona
+    {
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+
+        public string Apellido
+        {
+            set { ValidarNombreApellido(apellido); }
+            get { return apellido; }
+        }
+        public string Nombre
+        {
+            set { ValidarNombreApellido(nombre); }
+            get { return nombre; }
+        }
+
+        private string ValidarNombreApellido(string dato)
+        {
+            string retorno = dato;
+            for(int i = 0;i<dato.Length;i++)
+            {
+                if (dato[i] < '0' && dato[i] > '9')
+                {
+                    retorno = "";
+                    break;
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ TP3/Valdiviezo.Lucas.2C.TP3/Excepciones/DniInvalidoException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class DniInvalidoException : Exception
+    {
+        private string mensajeBase;
+
+        public DniInvalidoException() : this("Error",null)
+        {
+          
+        }
+
+        public DniInvalidoException(Exception e) : this("Error",e)
+        {
+
+        }
+
+        public DniInvalidoException(string message) : this(message,null)
+        {
+
+        }
+        public DniInvalidoException(string message, Exception e) : base(message,e)
+        {
+            mensajeBase = message;
+        }
+    }
+}
