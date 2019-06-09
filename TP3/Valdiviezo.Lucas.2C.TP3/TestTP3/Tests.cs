18f09d8fe6b80cee382e02e8a3b96aa60ddf9e9e
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-06-09 18:56:29 -0300

    TP3 LISTO

--- TP3/Valdiviezo.Lucas.2C.TP3/TestTP3/Tests.cs
+++ TP3/Valdiviezo.Lucas.2C.TP3/TestTP3/Tests.cs
@@ -52,15 +52,43 @@ namespace TestTP3
             }
         }
         /// <summary>
-        /// Comprueba que salte la Excepcion Alumno Repetido al crear 2 alumnos iguales e intentar agregarlos a la universidad
+        /// Comprueba que el DNI se haya ingresado y validado correctamente.
         /// </summary>
         [TestMethod]
         public void TestValorNumerico()
         {
             //arrange
-            Alumno a1 = new Alumno(1, "Carlos", "Chau","12345678", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Alumno a1 = new Alumno(1,"Mario","Juarez", "12345678",Persona.ENacionalidad.Argentino,Universidad.EClases.Laboratorio);
             //assert
-            
+            Assert.IsTrue(12345678 == a1.DNI);
+        }
+        /// <summary>
+        /// Comprueba que el objeto no tenga ninguno de sus atributos en NULL
+        /// </summary>
+        [TestMethod]
+        public void TestValorNoNull()
+        {
+            //arrange
+            Alumno a1 = new Alumno(1, "Mario", "Juarez", "12345678", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Profesor p1 = new Profesor(1, "Mario", "Juarez", "12345678", Persona.ENacionalidad.Argentino);
+            Jornada j1 = new Jornada(Universidad.EClases.Laboratorio, p1);
+            //act
+            j1 += a1;
+            //assert
+            //Alumno
+            Assert.IsNotNull(a1.DNI);
+            Assert.IsNotNull(a1.Nacionalidad);
+            Assert.IsNotNull(a1.Nombre);
+            Assert.IsNotNull(a1.Apellido);
+            //Profesor
+            Assert.IsNotNull(p1.DNI);
+            Assert.IsNotNull(p1.Nacionalidad);
+            Assert.IsNotNull(p1.Nombre);
+            Assert.IsNotNull(p1.Apellido);
+            //Jornada
+            Assert.IsNotNull(j1.Alumnos);
+            Assert.IsNotNull(j1.Clase);
+            Assert.IsNotNull(j1.Instructor);
         }
 
     }
