e83ad38525448c222e4fad74459a51b533312935
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-06-09 03:54:43 -0300

    Clase universidad comentada

--- TP3/Valdiviezo.Lucas.2C.TP3/ClasesInstanciables/Universidad.cs
+++ TP3/Valdiviezo.Lucas.2C.TP3/ClasesInstanciables/Universidad.cs
@@ -82,7 +82,7 @@ namespace ClasesInstanciables
         /// Metodo privado y de clase el cual recibe una universidad y devuelve un string con sus datos.
         /// <param name="uni">Universidad de la cual se devolveran los datos. </param>
         /// </summary>
-        /// <returns>String con los datos del alumno.</returns>
+        /// <returns>String con los datos de la universidad.</returns>
         private static string MostrarDatos(Universidad uni)
         {
             StringBuilder mostrar = new StringBuilder();
@@ -101,12 +101,19 @@ namespace ClasesInstanciables
 
             return mostrar.ToString();
         }
-
+        /// <summary>
+        /// Sobrecarga del metodo ToString la cual puede ser llamada fuera de la clase y devuelve los datos de la universidad.
+        /// </summary>
+        /// <returns>String con los datos de la universidad.</returns>
         public override string ToString()
         {
             return MostrarDatos(this);
         }
-
+        /// <summary>
+        /// Metodo Guardar de clase, el cual guarda un archivo serializado con los datos de la universidad.
+        /// <param name="uni">Datos de la universidad a guardar. </param>
+        /// </summary>
+        /// <returns>True si se guardo el archivo con exito o false si ocurrio una excepcion y no se guardo.</returns>
         public static bool Guardar(Universidad uni)
         {
             bool retorno = false;
@@ -123,7 +130,10 @@ namespace ClasesInstanciables
 
             return retorno;
         }
-
+        /// <summary>
+        /// Metodo Guardar de clase, el cual lee un archivo serializado con los datos de la universidad.
+        /// </summary>
+        /// <returns>Devuelve un objeto del tipo Universidad con su datos, devuelve el objeto Universidad vacio sino se logro leer.</returns>
         public static Universidad Leer()
         {
 
@@ -142,11 +152,12 @@ namespace ClasesInstanciables
 
             return retorno;
         }
-
-        
-        
-
-
+        /// <summary>
+        /// Sobrecarga del operador == que verifica que profesor esta dando una clase en especifico.
+        /// <param name="g">Universidad con el profesor a comparar. </param>
+        /// <param name="clase">Clase a comparar. </param>
+        /// </summary>
+        /// <returns>El primer profesor que de la clase, lanza una excepcion si ningun profesor da esa clase.</returns>
         public static Profesor operator ==(Universidad g, EClases clase)
         {
             foreach (Profesor p in g.Profesores)
@@ -159,7 +170,12 @@ namespace ClasesInstanciables
 
             throw new SinProfesorException();
         }
-
+        /// <summary>
+        /// Sobrecarga del operador != que verifica que profesor no esta dando una clase en especifico.
+        /// <param name="g">Universidad con el profesor a comparar. </param>
+        /// <param name="clase">Clase a comparar. </param>
+        /// </summary>
+        /// <returns>El primer profesor que no de la clase, lanza una excepcion si ningun profesor no da esa clase.</returns>
         public static Profesor operator !=(Universidad g, EClases clase)
         {
             foreach (Profesor p in g.Profesores)
@@ -172,7 +188,12 @@ namespace ClasesInstanciables
 
             throw new SinProfesorException();
         }
-
+        /// <summary>
+        /// Sobrecarga del operador == que verifica que el profesor pertenezca a la universidad.
+        /// <param name="g">Universidad a comparar. </param>
+        /// <param name="i">Profesor a comparar. </param>
+        /// </summary>
+        /// <returns>True si el profesor pertenece a la universidad, false si no pertenece.</returns>
         public static bool operator ==(Universidad g, Profesor i)
         {
             bool retorno = false;
@@ -187,12 +208,22 @@ namespace ClasesInstanciables
 
             return retorno;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador != que verifica que el profesor no pertenezca a la universidad.
+        /// <param name="g">Universidad a comparar. </param>
+        /// <param name="i">Profesor a comparar. </param>
+        /// </summary>
+        /// <returns>True si el profesor no pertenece a la universidad, false si pertenece.</returns>
         public static bool operator !=(Universidad g, Profesor i)
         {
             return !(g==i);
         }
-
+        /// <summary>
+        /// Sobrecarga del operador == que verifica que el alumno pertenezca a la universidad.
+        /// <param name="g">Universidad a comparar. </param>
+        /// <param name="a">Alumno a comparar. </param>
+        /// </summary>
+        /// <returns>True si el alumno pertenece a la universidad, false si no pertenece.</returns>
         public static bool operator ==(Universidad g, Alumno a)
         {
             bool retorno = false;
@@ -207,12 +238,22 @@ namespace ClasesInstanciables
 
             return retorno;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador != que verifica que el alumno no pertenezca a la universidad.
+        /// <param name="g">Universidad a comparar. </param>
+        /// <param name="a">Alumno a comparar. </param>
+        /// </summary>
+        /// <returns>True si el alumno no pertenece a la universidad, false si pertenece.</returns>
         public static bool operator !=(Universidad g, Alumno a)
         {
             return !(g==a);
         }
-
+        /// <summary>
+        /// Sobrecarga del operador + que agrega un alumno a la universidad si este no forma parte de ella.
+        /// <param name="g">Universidad donde se agregara. </param>
+        /// <param name="a">Alumno a agregar. </param>
+        /// </summary>
+        /// <returns>Retorna la universidad con o sin cambios si se agrego o no el alumno.</returns>
         public static Universidad operator +(Universidad u, Alumno a)
         {
             if (u != a)
@@ -226,7 +267,12 @@ namespace ClasesInstanciables
             }
             return u;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador + que agrega un profesor a la universidad si este no forma parte de ella.
+        /// <param name="g">Universidad donde se agregara. </param>
+        /// <param name="a">Profesor a agregar. </param>
+        /// </summary>
+        /// <returns>Retorna la universidad con o sin cambios si se agrego o no el profesor.</returns>
         public static Universidad operator +(Universidad u, Profesor i)
         {
             if (u != i)
@@ -235,7 +281,12 @@ namespace ClasesInstanciables
             }
             return u;
         }
-
+        /// <summary>
+        /// Sobrecarga del operador + que agregara una jornada nueva agregandole un Profesor y alumnos que tengan la misma clase.
+        /// <param name="g">Universidad donde se agregara. </param>
+        /// <param name="clase">Clase de la Jornada a crear </param>
+        /// </summary>
+        /// <returns>Retorna la universidad con o sin cambios si se agrego o no la jornada.</returns>
         public static Universidad operator +(Universidad g, EClases clase)
         {
             Jornada jornada = new Jornada(clase, g == clase);
