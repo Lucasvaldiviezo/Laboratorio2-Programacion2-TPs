c48b5932265398336ecd0fb8abfee78efee507c1
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-06-09 17:02:44 -0300

    Test casi listos

---
+++ TP3/Valdiviezo.Lucas.2C.TP3/TestTP3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTP3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTP3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7800b096-21ea-4c22-ae08-8cea3450e192")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP3/Valdiviezo.Lucas.2C.TP3/TestTP3/Tests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClasesAbstractas;
+using ClasesInstanciables;
+using Excepciones;
+using Archivos;
+
+namespace TestTP3
+{
+    [TestClass]
+    public class Tests
+    {
+        /// <summary>
+        /// Comprueba que salte la Excepcion Alumno Repetido al crear 2 alumnos iguales e intentar agregarlos a la universidad.
+        /// </summary>
+        [TestMethod]
+        public void TestAlumnoRepetidoException()
+        {
+            //arrange
+            Universidad uni = new Universidad();
+            Alumno a1 = new Alumno(1, "Juan", "Torto", "10000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            Alumno a2 = new Alumno(2, "Carlos", "Rodriguez", "10000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            //act
+            try
+            {
+                uni += a1;
+                uni += a2;
+            }
+            catch (AlumnoRepetidoException e)
+            {
+                //assert
+                Assert.IsInstanceOfType(e, typeof(AlumnoRepetidoException));
+            }
+        }
+        /// <summary>
+        /// Comprueba que salte la Excepcion Alumno Repetido al crear 2 alumnos iguales e intentar agregarlos a la universidad.
+        /// </summary>
+        [TestMethod]
+        public void TestSinProfesorException()
+        {
+            //arrange
+            Universidad uni = new Universidad();
+            //act
+            try
+            {
+                uni += Universidad.EClases.Legislacion;
+            }
+            catch (SinProfesorException e)
+            {
+                //assert
+                Assert.IsInstanceOfType(e, typeof(SinProfesorException));
+            }
+        }
+        /// <summary>
+        /// Comprueba que salte la Excepcion Alumno Repetido al crear 2 alumnos iguales e intentar agregarlos a la universidad
+        /// </summary>
+        [TestMethod]
+        public void TestValorNumerico()
+        {
+            //arrange
+            Alumno a1 = new Alumno(1, "Carlos", "Chau","12345678", Persona.ENacionalidad.Argentino, Universidad.EClases.Laboratorio);
+            //assert
+            
+        }
+
+    }
+}
