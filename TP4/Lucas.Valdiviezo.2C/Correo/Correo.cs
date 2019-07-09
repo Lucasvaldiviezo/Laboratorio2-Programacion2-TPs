7359fd27b87d523bf8370c880b713d3f3f0ae9f7
Lucasvaldiviezo <lucasvaldiviezo80@gmail.com>
2019-07-08 23:51:13 -0300

    TP4 continuacion

--- TP4/Lucas.Valdiviezo.2C/Correo/Correo.cs
+++ TP4/Lucas.Valdiviezo.2C/Correo/Correo.cs
@@ -7,9 +7,20 @@ using System.Threading;
 
 namespace Entidades
 {
-    public class Correo
+    public class Correo : IMostrar<List<Paquete>>
     {
         private List<Thread> mockPaquetes;
         private List<Paquete> paquetes;
+
+        public Correo()
+        {
+            mockPaquetes = new List<Thread>();
+            paquetes = new List<Paquete>();
+        }
+
+        public static Correo operator +(Correo c, Paquete p)
+        {
+            
+        }
     }
 }
